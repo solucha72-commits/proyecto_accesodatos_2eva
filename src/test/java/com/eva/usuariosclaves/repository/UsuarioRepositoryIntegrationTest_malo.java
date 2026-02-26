3f638bb008f6163a8ade0ec277b8101b1df303b6
santiagoolsa <137781282+santiagoolsa@users.noreply.github.com>
2026-02-26 16:00:32 +0100

    otro cambio

--- src/test/java/com/eva/usuariosclaves/repository/UsuarioRepositoryIntegrationTest_malo.java
+++ src/test/java/com/eva/usuariosclaves/repository/UsuarioRepositoryIntegrationTest_malo.java
@@ -5,10 +5,6 @@ import com.eva.usuariosclaves.model.Usuario;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
@@ -17,21 +13,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @DataJpaTest
-class UsuarioRepositoryIntegrationTest {
+class UsuarioRepositoryIntegrationTest_malo {
 
-    @Container
-    static PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:15")
-                    .withDatabaseName("testdb")
-                    .withUsername("test")
-                    .withPassword("test");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
+//    @Container
+//    static PostgreSQLContainer<?> postgres =
+//            new PostgreSQLContainer<>("postgres:15")
+//                    .withDatabaseName("testdb")
+//                    .withUsername("test")
+//                    .withPassword("test");
+//
+//    @DynamicPropertySource
+//    static void configureProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+//        registry.add("spring.datasource.username", postgres::getUsername);
+//        registry.add("spring.datasource.password", postgres::getPassword);
+//    }
 
     @Autowired
     private UsuarioRepository usuarioRepository;
